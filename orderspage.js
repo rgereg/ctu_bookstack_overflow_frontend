6864d85ddaf1695a84dbee806b730058f84144e8
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-01-30 05:54:11 -0500

    Starting orders page

---
+++ orderspage.js
@@ -0,0 +1,59 @@
+import { initAuth, userRole, apiFetch } from "./loginpage.js";
+
+const API_BASE = "https://ctu-bookstack-overflow-backend.onrender.com";
+
+const main = document.getElementById("main");
+
+let orderList = [];
+let session = null;
+
+async function initPage() {
+  session = await initAuth();
+
+  if (!session || userRole !== "customer") {
+    main.innerHTML = "<div id='message'><p>Please log in as a customer to view your cart.</p></div>";
+    return;
+  }
+  else {
+        await loadCart();
+  }
+}
+
+
+async function loadCart() {
+    try {
+      const res = await apiFetch(`/orders`);
+      orderList = await res.json();
+      console.log(orderList);
+      renderOrders(orderList);
+    } catch (err) {
+      console.error(err);
+      main.innerHTML = "<div id='message'><p>Error loading orders.</p></div>";
+    }
+}
+
+function renderOrders(data) {
+    main.innerHTML = "";
+
+    if (!data.length) {
+        main.innerHTML = "<div id='message'><p>No orders found</p></div>";
+        return;
+    }
+
+    data.forEach(item => {
+        const div = document.createElement("div");
+        div.className = "item";
+
+        div.innerHTML = `
+        <div class="itemnonimage">
+            <h1>Order ID: ${item.id}</h1>
+            <h2>Customer ID: ${item.customer_id}</h2>
+            <h3>Status: ${item.status}</h3>
+        </div>
+        `;
+
+        main.appendChild(div);
+    });
+}
+
+initPage();
