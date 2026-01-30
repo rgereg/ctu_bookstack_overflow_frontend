79dce99e3dbd30bd7adde470ad92b6935c0d02f2
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-01-30 00:28:41 -0500

    Separating load and render for orders on cart page

--- cartpage.js
+++ cartpage.js
@@ -5,15 +5,15 @@ const API_BASE = "https://ctu-bookstack-overflow-backend.onrender.com";
 const main = document.getElementById("main");
 
 let currentOrder = [];
-let session = null;
+let userRole = null;
 
 
 // Going to be messing with this more, using old cart loading function that was in the HTML for the page
 
 async function initPage() {
-  session = await initAuth();
+  userRole = await initAuth();
 
-  if (!session || userRole !== "customer") {
+  if (!userRole || userRole !== "customer") {
     main.innerHTML = "<div id='message'><p>Please log in as a customer to view your cart.</p></div>";
     return;
   }
@@ -23,60 +23,37 @@ async function initPage() {
 }
 
 async function loadCart() {
-  if (!session || userRole !== "customer") {
-    main.innerHTML = "<div id='message'><p>Please log in as a customer to view your cart.</p></div>";
-    return;
-  }
-
-  try {
-    const res = await fetch(`${API_BASE}/cart`);
-    const currentOrder = await res.json();
-
-    if (currentOrder.length === 0) {
-      main.innerHTML = "<div id='message'><p>Your cart is empty.</p></div>";
-      return;
-    }
-
-    //let html = `
-    //    <table>
-    //    <thead>
-    //        <tr>
-    //        <th>Book</th>
-    //        <th>Quantity</th>
-    //        </tr>
-    //    </thead>
-    //    <tbody>
-    //`;
-
-    //cartOrders.forEach(order => {
-    //    html += `
-    //    <tr>
-    //        <td>${order.book_title}</td>
-    //        <td>${order.quantity}</td>
-    //    </tr>
-    //    `;
-    //});
-
-    //html += "</tbody></table>";
-    //main.innerHTML = html;
-
-    currentOrder.forEach(order => {
-        const div = document.createElement("div");
-        div.className = "item";
-        div.innerHTML = `
-        <div class="itemnonimage">
-          <h1>${order.id}</h1>
-          <h2>${order.customer_id}</h2>
-        </div>
-        `
-
-        main.appendChild(div);
-    });
-
+    try {
+      const res = await apiFetch(`/cart`);
+      currentOrder = await res.json();
+      renderOrders(currentOrder);
     } catch (err) {
       console.error(err);
       main.innerHTML = "<div id='message'><p>Error loading cart.</p></div>";
     }
+}
+
+function renderOrders(data) {
+    main.innerHTML = "";
+
+    if (!data.length) {
+        main.innerHTML = "<div id='message'><p>No orders found</p></div>";
+        return;
+    }
+
+    data.forEach(order => {
+        const div = document.createElement("div");
+        div.className = "item";
+
+        div.innerHTML = `
+        <div class="itemnonimage">
+            <h1>${order.id}</h1>
+            <h2>${order.customer_id}</h2>
+        </div>
+        `;
+
+        main.appendChild(div);
+    });
 }
 
 initPage();
