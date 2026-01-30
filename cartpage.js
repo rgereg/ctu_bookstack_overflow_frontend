b778938bd949ebcd785ada604072048932a17748
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-01-30 00:48:10 -0500

    Update cartpage.js

--- cartpage.js
+++ cartpage.js
@@ -10,7 +10,7 @@ let session = null;
 // Going to be messing with this more, using old cart loading function that was in the HTML for the page
 
 async function initPage() {
-  await initAuth();
+  session = await initAuth();
 
   if (!session || userRole !== "customer") {
     main.innerHTML = "<div id='message'><p>Please log in as a customer to view your cart.</p></div>";
