02a97ea6b350cc801b9b45d4d73f34a174d89195
rgereg <64085302+rgereg@users.noreply.github.com>
2026-02-02 18:51:09 -0500

    add temp debug line for testing

--- loginpage.js
+++ loginpage.js
@@ -55,6 +55,8 @@ export async function initAuth() {
   const authEmailDisplay = document.getElementById("authEmailDisplay"); // CHANGED:
 
   if (session) {
+    console.log("AUTH ROLE =", session.user?.user_metadata?.role); // CHANGED:
+
     loginBtn?.classList.add("hidden");
     logoutBtn?.classList.remove("hidden");
     loginFormContainer?.classList.add("hidden");
